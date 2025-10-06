49dc17c6601a7652583be9559ece358717c4b2e5
William Philipp <william_philipp@web.de>
2025-10-06 15:26:00 +0200

    remove grader model from meta eval class

--- healthbench_meta_eval.py
+++ healthbench_meta_eval.py
@@ -36,7 +36,6 @@ HEALTHBENCH_META_HTML_JINJA = (
 class HealthBenchMetaEval(Eval):
     def __init__(
         self,
-        grader_model: SamplerBase,
         num_examples: int | None = None,
         n_threads: int = 120,
         n_repeats: int = 1,
@@ -52,7 +51,6 @@ class HealthBenchMetaEval(Eval):
 
         self.examples = examples * n_repeats
         self.n_threads = n_threads
-        self.grader_model = grader_model
 
     def grade_sample(
         self,
